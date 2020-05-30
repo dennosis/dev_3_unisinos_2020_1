5dc4eaab19c1b7a948da4474d79944f6174b82cc
Nícolas Bruno <nicolas_bruno@terceiros.sicredi.com.br>
2020-05-30 16:28:14 -0300

    added complex filtters

--- app/controllers/car/car.js
+++ app/controllers/car/car.js
@@ -1,6 +1,7 @@
 const Car = require('../../models/car');
 const App = require('../../models/app');
 const AsyncUtils = require('../../../utils-module').Async;
+const FiltterBuilderUtils = require('../../../utils-module').FiltterBuilder;
 
 module.exports = {
     create : async (req, res) =>{
@@ -34,14 +35,35 @@ module.exports = {
     },
 
     search : async (req, res) => {
-        const { manufactureYear, modelYear } = req.body;
+        const { name, apps, brand, model, manufactureYear, modelYear, cost, luggages, airConditioner, passengers } = req.body;
 
         let filtters = {};
-        if (manufactureYear) { filtters.manufactureYear = manufactureYear}
-        if (modelYear) { filtters.modelYear = modelYear}
         
-        const cars = await Car.find(filtters)
+        //simple filtter
+        if (name) { filtters.name = name}
+        if (brand) { filtters.brand = brand}
+        if (model) { filtters.model = model}
+        if (luggages) { filtters.luggages = luggages}
+        if (airConditioner) { filtters.airConditioner = airConditioner}
+
+        //complex apps filtter
+        if (apps) { filtters.apps = { $in: apps } }
+
+        //complex filtter
+        if (manufactureYear) { filtters = FiltterBuilderUtils.build(manufactureYear, filtters, "manufactureYear") }
+        if (modelYear) { filtters = FiltterBuilderUtils.build(modelYear, filtters, "modelYear") }
+        if (cost) { filtters = FiltterBuilderUtils.build(cost, filtters, "cost") }
+        if (passengers) { filtters.passengers = passengers}
+        
+        let cars = [];
+        
+        try {
+            cars = await Car.find(filtters)
+        } catch (ex) {
+            console.log(ex);
+        }
         
         return res.send(cars)
     }
 }
+
--- utils-module/index.js
+++ utils-module/index.js
@@ -1,3 +1,5 @@
 let asyncUtils = require('./lib/async');
+let filtterBuilder = require('./lib/filtterBuilder');
 
 exports.Async = asyncUtils;
+exports.FiltterBuilder = filtterBuilder;
